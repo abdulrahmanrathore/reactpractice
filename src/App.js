3201569955de8ffa93b6719eaaebc53af4a86e78
Abdul Rahman Rathore <abdulrahmanrathore2@gmail.com>
2023-06-15 18:30:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import "./styles.css";
+import {useState} from 'react';
+
+
+const countries = [
+  {name:'India', value:'IN', cities:[
+    'Delhi',
+    'Mumbai'
+  ]},
+  {name:'Pak', value:'PK', cities:[
+    'Lahore',
+    'Karanchi'
+  ]},
+  {name:'Bangladesh', value:'BG', cities:[
+    'Dhaka',
+    'Chittagong'
+  ]},
+];
+
+export default function App() {
+
+
+  const [country, setCountry] = useState({name:'', value:'', cities:[]});
+
+  return (
+    <div className="App">
+      <select onChange={(e) => {
+        // console.log(e.target.value);
+        setCountry(e.target.value);
+      }}>
+        {
+          countries.map((item, index) => {
+            return <option value={index}>{item.name}</option>
+          })
+        }
+      </select>
+
+      <select value={country}>
+        { 
+          countries[country].cities.map((item, index) => {
+            return <option value={index}>{item}</option>
+          })
+        }
+      </select>
+    </div>
+  );
+}
